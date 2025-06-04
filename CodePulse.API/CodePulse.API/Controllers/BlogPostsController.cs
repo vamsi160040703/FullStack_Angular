cf014bfcfefebd28ab814647e7712385ae092874
VAMSI KRISHNA <pellurivamsi@hotmail.com>
2025-06-04 23:06:11 +0530

    Implemented Post operation for blogposts

---
+++ CodePulse.API/CodePulse.API/Controllers/BlogPostsController.cs
@@ -0,0 +1,56 @@
+﻿using CodePulse.API.Models.Domain;
+using CodePulse.API.Models.DTO;
+using CodePulse.API.Repositories.Implementation;
+using CodePulse.API.Repositories.Interface;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace CodePulse.API.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class BlogPostsController : ControllerBase
+    {
+        private readonly IBlogPostRepository blogPostRepository;
+        public BlogPostsController(IBlogPostRepository blogPostRepository)
+        {
+            this.blogPostRepository = blogPostRepository; 
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> CreateBlogPost([FromBody] CreateBlogPostRequestDto request)
+        {
+            // Convert DTO to DOmain
+            var blogPost = new BlogPost
+            {
+                Id = Guid.NewGuid(),
+                Title = request.Title,
+                ShortDescription = request.ShortDescription,
+                Content = request.Content,
+                FeaturedImageUrl = request.FeaturedImageUrl,
+                UrlHandle = request.UrlHandle,
+                PublishedDate = request.PublishedDate,
+                Author = request.Author,
+                IsVisible = request.IsVisible
+            };
+            blogPost = await blogPostRepository.CreateAsync(blogPost);
+
+            // Convert Domain Model back to DTO
+            var response = new BlogPostDto
+            {
+                Id = blogPost.Id,
+                Author = blogPost.Author,
+                Content = blogPost.Content,
+                FeaturedImageUrl = blogPost.FeaturedImageUrl,
+                IsVisible = blogPost.IsVisible,
+                PublishedDate = blogPost.PublishedDate,
+                ShortDescription = blogPost.ShortDescription,
+                Title = blogPost.Title,
+                UrlHandle = blogPost.UrlHandle,
+            };
+            return Ok(response);
+        }
+    }
+        
+}
